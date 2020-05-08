b74e133d9bf171d879df48c58ed4ab65e3acc86a
Marcos Vinício <marcosviniciohd@hotmail.com>
2020-05-08 20:02:15 -0300

    Leitura dos dados

--- projeto-bar/src/br/com/marcosviniciohd/Programa.java
+++ projeto-bar/src/br/com/marcosviniciohd/Programa.java
@@ -1,10 +1,33 @@
 package br.com.marcosviniciohd;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		char sexo;
+		int cervejas, refri, espetos;
+		
+		
+		System.out.print("Sexo ");
+		sexo = sc.next().charAt(0);
+		System.out.print("Quantidade de cervejas ");
+		cervejas = sc.nextInt();
+		System.out.print("Quantidade de refrigerantes ");
+		refri = sc.nextInt();
+		System.out.print("Quantidade de espetinhos ");
+		espetos = sc.nextInt();
+		
+		
+		
+		
+		sc.close();
+		
 	}
 
 }
