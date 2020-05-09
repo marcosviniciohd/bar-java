f09b904606f541a48356aecad867813cf135e6c3
Marcos Vinício <marcosviniciohd@hotmail.com>
2020-05-08 21:43:35 -0300

    Relatorio gerado

--- projeto-bar/src/br/com/marcosviniciohd/Programa.java
+++ projeto-bar/src/br/com/marcosviniciohd/Programa.java
@@ -12,20 +12,48 @@ public class Programa {
 		
 		char sexo;
 		int cervejas, refri, espetos;
-		
-		
-		System.out.print("Sexo ");
+		double consumo, couvert, ingresso, total;
+		// 1º Leitura dos dados
+		System.out.print( "Sexo " );
 		sexo = sc.next().charAt(0);
-		System.out.print("Quantidade de cervejas ");
+		System.out.print( "Quantidade de cervejas " );
 		cervejas = sc.nextInt();
-		System.out.print("Quantidade de refrigerantes ");
+		System.out.print( "Quantidade de refrigerantes " );
 		refri = sc.nextInt();
-		System.out.print("Quantidade de espetinhos ");
+		System.out.print( "Quantidade de espetinhos " );
 		espetos = sc.nextInt();
 		
+		//2º Calculos
 		
+		if ( sexo == 'F' ) {
+			ingresso = 8.0;
+		}else {
+			ingresso = 10.0;
+		}
 		
+		consumo = cervejas * 5.0 + refri * 3.0 + espetos * 7.0;
 		
+		if ( consumo > 30 ) {
+			couvert = 0.0;
+		}else {
+			couvert = 4.0;
+		}
+		
+		total = ingresso + consumo + couvert;
+		
+		//3º Relatório
+		
+		System.out.println();
+		System.out.println( "Relatório:" );
+		System.out.printf( "Consumo = R$ %.2f%n", consumo );
+		if (couvert == 0.0) {
+			System.out.println( "Isento de couvert" );
+		}else {
+			System.out.printf( "Couvert = R$ %.2f%n", couvert );
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
 		sc.close();
 		
 	}
